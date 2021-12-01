89b9668ee8e653a15143d1b7b98fc684f8c44ef0
pgm-michielwillems <michiel.willems@student.arteveldehs.be>
2021-12-01 16:09:38 +0100

    worked on filter for arts

--- static/js/art-and-exhibitions.js
+++ static/js/art-and-exhibitions.js
@@ -17,34 +17,40 @@
         this.generateFilterForAll();
       },
       generateListOfArts (highlights) {
-            return highlights.map((head) => {
-              for (let i = 0; i < head.images.length; i++) {
-              return `
+        return highlights.map((head) => {
+          for (let i = 0; i < head.images.length; i++) {
+          return `
+            <article class="box-article">
+              <div class="box-text">
                 <article class="box-articles">
-                  <a href="art-and-exhibitions/detail/index.html">${head.title}</a>
-                  <div class="tags-grey">${head.tags} ${head.location === null ? head.location = "" : head.location}</div>
-                  <div class="box-images">
-                    <img src="static/img/afbeeldingen/${head.images[i]}" loading="lazy" alt="${head.subtitle}">
-                  </div>
-                </article>`
-              }
-          }).join(' ');
+                <h2 class="a-art--black"><a href="art-and-exhibitions/detail/index.html">${head.title}</a></h2>
+                <h3>${head.subtitle}</h3>
+                <p class="tags-grey">${head.tags} ${head.location === null ? head.location = "" : head.location}</p>
+              </div>
+              <div class="box-images--art">
+                <img src="static/img/afbeeldingen/${head.images[i]}" loading="lazy" alt="${head.subtitle}">
+              </div>
+            </article>`
+          }
+        }).join('');
       },
       generateFilterForAll () {
-          for (const $filter of this.$categoryItems) {
-            $filter.addEventListener('click',() => {
-              const category = $filter.dataset.category;
-              this.filterCategory = category;
-              this.filterYears = category;
-              this.generateFilteredCategory();
-            }, false);
-          }
+        for (const $filter of this.$categoryItems) {
+          $filter.addEventListener('click',() => {
+            const category = $filter.dataset.category;
+            this.filterCategory = category;
+            this.filterYears = category;
+            this.generateFilteredCategory();
+          }, false);
+        }
       },
       generateFilteredCategory () {
         const items = ARTS.filter((item) => item.tags.includes(this.filterCategory));
         const years = ARTS.filter((year) => year.year === this.filterYears);
+        this.filterYears = `<div class="box-article box-article--year"><div class="box-text"><h2>${this.filterYears}</h2></div></div>`
           if (this.filterCategory != null) {
-          this.$filterAll.innerHTML = this.generateListOfArts(items);
+          this.$filterAll.innerHTML = this.filterYears
+          this.$filterAll.innerHTML += this.generateListOfArts(items);
           this.$filterAll.innerHTML += this.generateListOfArts(years);
         } else {
           this.$filterAll.innerHTML = this.generateListOfArts(ARTS);
